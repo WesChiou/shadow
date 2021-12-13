8582f351634b7e5038ffed1d598c838d82468169
Qiu Weishi <weishi@whut.edu.cn>
2021-12-13 21:43:18 +0800

    make buttons configurable

--- src/shadow-pagination/index.js
+++ src/shadow-pagination/index.js
@@ -4,6 +4,8 @@
     return !Number.isNaN(value - parseFloat(value));
   }
 
+  const SMART_MODE_ENUM = ['hidden', 'disabled'];
+
   class ShadowPagination extends HTMLElement {
     #value = 1;
 
@@ -12,42 +14,46 @@
       const shadowRoot = this.attachShadow({ mode: 'open' });
 
       shadowRoot.addEventListener('click', (event) => {
-        const type = event.target.dataset.page;
+        const button = event.target.closest('button');
+
+        if (!button || button.disabled) return;
+
+        const type = button.dataset.page;
         switch (type) {
           case 'first':
             this.value = 1;
             this.dispatchEvent(new Event('firstpage'));
             break;
-          case 'prev':
+          case 'previous':
             this.value -= 1;
-            this.dispatchEvent(new Event('prevpage'));
+            this.dispatchEvent(new Event('previouspage'));
             break;
           case 'next':
             this.value += 1;
             this.dispatchEvent(new Event('nextpage'));
             break;
           case 'last':
-            this.value = this.pages;
+            this.value = this.count;
             this.dispatchEvent(new Event('lastpage'));
             break;
           default:
-            this.value = type;
+            this.value = +type;
             break;
         }
       });
     }
 
-    get pages() {
-      const pagesAttr = this.getAttribute('pages');
+    get count() {
+      const pagesAttr = this.getAttribute('count');
       if (isNumeric(pagesAttr)) {
         return pagesAttr;
       }
       return 0;
     }
 
-    set pages(v) {
+    set count(v) {
       if (isNumeric(v) && v >= 0) {
-        this.setAttribute('pages', v);
+        this.setAttribute('count', v);
       }
     }
 
@@ -56,15 +62,15 @@
     }
 
     set value(v) {
-      if (!isNumeric(v) || !isNumeric(this.pages) || +this.#value === +v) {
+      if (!isNumeric(v) || !isNumeric(this.count) || +this.#value === +v) {
         return;
       }
 
-      if (v > 0 && v <= this.pages) {
-        this.#value = v;
+      if (v > 0 && v <= this.count) {
+        this.#value = +v;
       } else if (v <= 0 && this.circular) {
-        this.#value = this.pages;
-      } else if (v > this.pages && this.circular) {
+        this.#value = this.count;
+      } else if (v > this.count && this.circular) {
         this.#value = 1;
       } else {
         return; // avoid dispatchEvent and render
@@ -86,32 +92,138 @@
       }
     }
 
+    get showFirstButton() {
+      return this.hasAttribute('show-first-button');
+    }
+
+    set showFirstButton(v) {
+      if (v) {
+        this.setAttribute('show-first-button', '');
+      } else {
+        this.removeAttribute('show-first-button');
+      }
+      this.render();
+    }
+
+    get showLastButton() {
+      return this.hasAttribute('show-last-button');
+    }
+
+    set showLastButton(v) {
+      if (v) {
+        this.setAttribute('show-last-button', '');
+      } else {
+        this.removeAttribute('show-last-button');
+      }
+      this.render();
+    }
+
+    get hidePreviousButton() {
+      return this.hasAttribute('hide-previous-button');
+    }
+
+    set hidePreviousButton(v) {
+      if (v) {
+        this.setAttribute('hide-previous-button', '');
+      } else {
+        this.removeAttribute('hide-previous-button');
+      }
+      this.render();
+    }
+
+    get hideNextButton() {
+      return this.hasAttribute('hide-next-button');
+    }
+
+    set hideNextButton(v) {
+      if (v) {
+        this.setAttribute('hide-next-button', '');
+      } else {
+        this.removeAttribute('hide-next-button');
+      }
+      this.render();
+    }
+
+    get nextSmartMode() {
+      return this.getAttribute('next-smart-mode');
+    }
+
+    set nextSmartMode(v) {
+      this.setAttribute('next-smart-mode', v);
+    }
+
+    get previousSmartMode() {
+      return this.getAttribute('previous-smart-mode');
+    }
+
+    set previousSmartMode(v) {
+      this.setAttribute('previous-smart-mode', v);
+    }
+
     render() {
-      const { pages, value } = this || {};
+      const {
+        count,
+        value,
+        showFirstButton,
+        showLastButton,
+        hidePreviousButton,
+        hideNextButton,
+        nextSmartMode,
+        previousSmartMode,
+      } = this || {};
+
+      function makePreviousButton() {
+        if (hidePreviousButton) {
+          return '';
+        }
+
+        let attr = '';
+        if (SMART_MODE_ENUM.includes(previousSmartMode)) {
+          attr = previousSmartMode;
+        }
+
+        return `<button data-page="previous" ${+value === 1 ? attr : ''}>
+                  <slot name="previous">&#706;</slot>
+                </button>`;
+      }
 
       function makePages() {
         let innerHTML = '';
-        if (isNumeric(pages)) {
-          for (let i = 1; i <= parseInt(pages, 10); i += 1) {
+        if (isNumeric(count)) {
+          for (let i = 1; i <= parseInt(count, 10); i += 1) {
             innerHTML += `<button data-page="${i}" class="page ${+value === i ? 'active' : ''}">${i}</button>`;
           }
         }
         return innerHTML;
       }
 
+      function makeNextButton() {
+        if (hideNextButton) {
+          return '';
+        }
+
+        let attr = '';
+        if (SMART_MODE_ENUM.includes(nextSmartMode)) {
+          attr = nextSmartMode;
+        }
+
+        return `<button data-page="next" ${+value === +count ? attr : ''}>
+                  <slot name="next">&#707;</slot>
+                </button>`;
+      }
+
       const template = document.createElement('template');
       template.innerHTML = `
         <style>
           :host { display: block; }
-          #pages-box { display: inline-block; }
           .page.active { background-color: #ffffff; }
         </style>
         <div>
-          <button data-page="first" class="first">&lt;&lt;</button>
-          <button data-page="prev" class="prev">&lt;</button>
-          <div id="pages-box">${makePages()}</div>
-          <button data-page="next" class="next">&gt;</button>
-          <button data-page="last" class="last">&gt;&gt;</button>
+          ${showFirstButton ? '<button data-page="first">&larrb;</button>' : ''}
+          ${makePreviousButton()}
+          ${makePages()}⁨⁨
+          ${makeNextButton()}
+          ${showLastButton ? '<button data-page="last">&rarrb;</button>' : ''}
         </div>
       `;
 
@@ -124,11 +236,20 @@
     }
 
     disconnectedCallback() {
-      console.log(this);
+
     }
 
     static get observedAttributes() {
-      return ['pages', 'value'];
+      return [
+        'count',
+        'value',
+        'show-first-button',
+        'show-last-button',
+        'hide-previous-button',
+        'hide-next-button',
+        'next-smart-mode',
+        'previous-smart-mode',
+      ];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -142,7 +263,7 @@
     }
 
     adoptedCallback() {
-      console.log(this);
+
     }
   }
 
